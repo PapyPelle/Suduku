9dedfb922526ec2cd2d73d937309e31a579a2547
Twifus <moukira45@gmail.com>
2019-12-15 10:19:12 +0100

    Improved general structure + sequential solving

--- generate.c
+++ generate.c
@@ -1,7 +1,6 @@
 // **************************
 // programme de F. Lemieux
-// usage : ./exe > grille.txt
-//   puis entrer n (scanf)
+// usage : ./exe 3 > grille.txt
 // **************************
 
 #include <stdio.h>
@@ -18,75 +17,77 @@ void print(int*, int); //Affiche une matrice n X n
 // L'entier 0 représente une case vide
 // Chaque bloc S est construit en permutant adéquatement les lignes et colonnes 
 // d'un bloc T fixe généré aléatoirement.
-int main(){
-    int i,j;
-    int n; // Le sudoku sera de dimension n^2 X n^2
-    int *T; //matrice nXn contenant les valeurs 1 à n^2
-    int *S; //matrice nXn contenant une permutation des lignes et colnnes de T
-    int *M; //matrice n^2 X n^2 : Sudoku
-    time_t secondes=time(NULL);
-    srand(secondes);
+int main(int argc, char* argv[]) {
+	int i, j;
+	int n; // Le sudoku sera de dimension n^2 X n^2
+	int* T; //matrice nXn contenant les valeurs 1 à n^2
+	int* S; //matrice nXn contenant une permutation des lignes et colnnes de T
+	int* M; //matrice n^2 X n^2 : Sudoku
+	time_t secondes = time(NULL);
+	srand(secondes);
 
-    scanf("%d", &n);
-    T= (int *) malloc(n*n*sizeof(int));
-    S= (int *) malloc(n*n*sizeof(int));
-    M= (int *) malloc(n*n*n*n*sizeof(int));
+	if (argc < 2) return EXIT_FAILURE;
+	n = atoi(argv[1]);
+	printf("%d\n", n);
 
-    // Initialisation aléatoire de la matrice T
-    // Cette matrice n'est jamais modifiée par a suite
-    for (i=0; i<n*n; i++) T[i]=i+1;
-    for (i=0; i<n*n; i++){
-        j=rand()%(n*n);
-        int x=T[j];
-        T[j]=T[i];
-        T[i]=x;
-    }
+	T = (int*)malloc(n * n * sizeof(int));
+	S = (int*)malloc(n * n * sizeof(int));
+	M = (int*)malloc(n * n * n * n * sizeof(int));
 
-    // On construit d'abord un Sudoku sans case libre
-    for (i=0;i<n; i++){
-        for (j=0; j<n; j++){
-            permute(T,S,n,i,j); 
-            injecte (S,M,n,i,j);
-        }
-    }
+	// Initialisation aléatoire de la matrice T
+	// Cette matrice n'est jamais modifiée par a suite
+	for (i = 0; i < n * n; i++) T[i] = i + 1;
+	for (i = 0; i < n * n; i++) {
+		j = rand() % (n * n);
+		int x = T[j];
+		T[j] = T[i];
+		T[i] = x;
+	}
 
-    // On remplace aléatoirement certains des éléments du Sudoku 
-    // par 0 dans une proportion de 1/2
-    for (i=0; i<n*n; i++)
-    for (j=0; j<n*n; j++)
-    if (rand()%2) M[i*n*n+j]=0;
-    print(M,n*n);
+	// On construit d'abord un Sudoku sans case libre
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			permute(T, S, n, i, j);
+			injecte(S, M, n, i, j);
+		}
+	}
 
-    return 0; // fin du main
+	// On remplace aléatoirement certains des éléments du Sudoku 
+	// par 0 dans une proportion de 1/2
+	for (i = 0; i < n * n; i++)
+		for (j = 0; j < n * n; j++)
+			if (rand() % 2) M[i * n * n + j] = 0;
+	print(M, n * n);
+
+	return 0; // fin du main
 }
 
 // Cette fonction permute les lignes et colonnes de T et place le résultat dans S.
 // T demeure inchangé
-void permute(int* T, int* S, int n, int a, int b){
-    int i,j;
-    for (i=0;i<n; i++)
-        for (j=0; j<n; j++) 
-            S[i*n+j]=T[((i+b)%n)*n + (j+a)%n];
+void permute(int* T, int* S, int n, int a, int b) {
+	int i, j;
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
+			S[i * n + j] = T[((i + b) % n) * n + (j + a) % n];
 }
 
 // Affiche la matrice S
-void print(int* S, int n){
-    int i,j;
-    for (i=0; i<n; i++){
-        for (j=0; j<n; j++)
-            printf("%d\t", S[i*n+j]);
-        printf("\n");
-    }
+void print(int* S, int n) {
+	int i, j;
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++)
+			printf("%d\t", S[i * n + j]);
+		printf("\n");
+	}
 }
 
 // Le Sudoku M est vu comme une grille nXn dont chaque élément 
 // est un bloc S de dimension nXn 
 // La fonction suivante insère la matrice S à la position (a,b) du Sudoku M
-void injecte(int *S, int* M, int n, int a, int b){
-    int i,j;
-    for (i=0; i<n; i++)
-        for (j=0; j<n; j++)
-            M[(n*n*n*a+n*n*i)+n*b+j]=S[i*n+j];
+void injecte(int* S, int* M, int n, int a, int b) {
+	int i, j;
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++) {
+			M[(n * n * n * a + n * n * i) + n * b + j] = S[i * n + j];
+		}
 }
-
-
---
+++ suduku2.c
@@ -0,0 +1,299 @@
+#define  _CRT_SECURE_NO_WARNINGS // remove scanf warnings on Windows
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct _cell
+{
+	char* values;
+	int values_size;
+	int count;
+	int unique_value;
+} Cell;
+
+typedef struct _sudoku
+{
+	Cell* data;
+	int dim;
+	int data_size;
+	int value_range;
+} Sudoku;
+
+typedef enum _group_type
+{
+	GROUP_ROW = 0,
+	GROUP_COLUMN,
+	GROUP_BLOCK,
+	LAST_GROUP_TYPE
+} GroupType;
+
+int		cell_init(Sudoku* s, Cell* c, int value);
+void	cell_free(Cell* c);
+void	cell_set_empty(Cell* c);
+void	cell_set_filled(Cell* c, int val);
+int		cell_has_unique_value(const Cell* c);
+int		cell_get_first_value(const Cell* c);
+int		cell_get_unique_value(const Cell* c);
+void	cell_set_value(Cell* c, int v);
+void	cell_unset_value(Cell* c, int v);
+
+Sudoku* sudoku_create(FILE* input);
+void	sudoku_free(Sudoku* s);
+Cell*	sudoku_get_cell(Sudoku* s, int row, int col);
+int		sudoku_get_cell_block(Sudoku* s, int row, int col);
+Cell*	sudoku_get_block_cell(Sudoku* s, int group, int index);
+void	sudoku_print(Sudoku* s);
+
+
+// =============================================================== //
+
+
+// Initialise une case avec une valeur donnée (0 si vide). Retourne 1 si réussi, 0 sinon.
+int cell_init(Sudoku* s, Cell* c, int value)
+{
+	c->values = (char*)malloc(s->value_range * sizeof(char));
+	if (c->values == NULL) return 0;
+	c->values_size = s->value_range;
+
+	if (value == 0)
+		cell_set_empty(c);
+	else
+		cell_set_filled(c, value);
+
+	return 1;
+}
+
+// Libère les ressources allouées pour une case. NE LIBERE PAS LA CASE EN ELLE MEME.
+void cell_free(Cell* c)
+{
+	free(c->values);
+	c->values = NULL;
+	c->values_size = 0;
+	c->count = 0;
+	c->unique_value = 0;
+}
+
+// Indique qu'une case peut contenir toutes les valeurs
+inline void cell_set_empty(Cell* c)
+{
+	memset(c->values, 1, c->values_size * sizeof(char));
+	c->count = c->values_size;
+	c->unique_value = 0;
+}
+
+// Indique qu'une case est occupée par une seule valeur
+inline void cell_set_filled(Cell* c, int val)
+{
+	memset(c->values, 0, c->values_size * sizeof(char));
+	c->values[val - 1] = 1;
+	c->count= 1;
+	c->unique_value = val;
+}
+
+// Renvoie 1 si la case possède un chiffre unique, 0 sinon
+inline int cell_has_unique_value(const Cell* c)
+{
+	return (c->count == 1);
+}
+
+// Renvoie la première valeur trouvée
+inline int cell_get_first_value(const Cell* c)
+{
+	int i;
+	for (i = 0; i < c->values_size; i++) {
+		if (c->values[i]) {
+			return i + 1;
+		}
+	}
+	return 0;
+}
+
+// Renvoie la valeure unique d'une case, 0 sinon
+inline int cell_get_unique_value(const Cell* c) {
+	if (!cell_has_unique_value(c))
+		return 0;
+	else
+		return c->unique_value;
+}
+
+// Indique que la case peut contenir la valeur n
+inline void cell_set_value(Cell* c, int v)
+{
+	if (c->values[v - 1] == 0)
+	{
+		c->values[v - 1] = 1;
+		if (c->count == 1)
+			c->unique_value = 0;
+		c->count++;
+	}
+}
+
+// Indique que la case ne peut pas contenir la valeur n
+inline void cell_unset_value(Cell* c, int v)
+{
+	if (c->values[v - 1] == 1)
+	{
+		c->values[v - 1] = 0;
+		c->count--;
+		if (c->count == 1)
+			c->unique_value = cell_get_first_value(c);
+	}
+}
+
+// Crée un sudoku à partir d'un fichier de données d'entrée
+Sudoku* sudoku_create(FILE* input)
+{
+	Sudoku* s;
+	int i;
+	int v;
+	int dim, value_range, data_size;
+
+	if (fscanf(input, "%d", &dim) <= 0) return NULL;
+
+	value_range = dim * dim;
+	data_size = value_range * value_range;
+
+	s = (Sudoku*)malloc(sizeof(Sudoku));
+	if (s == NULL) return NULL;
+
+	s->data = (Cell*)malloc(data_size * sizeof(Cell));
+	if (s->data == NULL) { free(s); return NULL; }
+	
+	s->dim = dim;
+	s->value_range = value_range;
+	s->data_size = data_size;
+
+	for (i = 0; i < s->data_size; i++)
+	{
+		if (fscanf(input, "%d", &v) <= 0 || !cell_init(s, &(s->data[i]), v)) { sudoku_free(s); return NULL; }
+	}
+
+	return s;
+}
+
+// Libère les ressources allouées par un sudoku
+void sudoku_free(Sudoku* s)
+{
+	free(s->data);
+	s->data = NULL;
+	s->data_size = 0;
+	free(s);
+}
+
+// Retourne la case du sudoku à la ligne/colonne indiquée
+inline Cell* sudoku_get_cell(Sudoku* s, int row, int col)
+{
+	return &(s->data[row * s->value_range + col]);
+}
+
+// Retourne une cellule d'une ligne
+inline Cell* sudoku_get_row_cell(Sudoku* s, int row, int index)
+{
+	return sudoku_get_cell(s, row, index);
+}
+
+// Retourne une cellule d'une colonne
+inline Cell* sudoku_get_column_cell(Sudoku* s, int col, int index)
+{
+	return sudoku_get_cell(s, index, col);
+}
+
+// Retourne l'identifiant du bloc contenant la case à la ligne/colonne indiqué
+inline int sudoku_get_cell_block(Sudoku* s, int row, int col)
+{
+	return (row / s->dim) * s->dim + (col / s->dim);
+}
+
+// Retourne une cellule d'un bloc
+inline Cell* sudoku_get_block_cell(Sudoku* s, int group, int index)
+{
+	return &(s->data[((group / s->dim) * s->dim + (index / s->dim)) * s->value_range + ((group % s->dim) * s->dim + (index % s->dim))]);
+}
+
+// Affiche la grille de sudoku
+void sudoku_print(Sudoku* s)
+{
+	printf("----------------\n");
+	int i, j;
+	int v;
+	for (i = 0; i < s->value_range; i++)
+	{
+		for (j = 0; j < s->value_range; j++)
+		{
+			v = cell_get_unique_value(sudoku_get_cell(s, i, j));
+			if (v != 0)
+				printf("%d ", v);
+			else
+				printf(". ");
+		}
+		printf("\n");
+	}
+}
+
+// Fonction interne mettant à jour les cases selon un *getter* donné.
+// Cf sudoku_update_{row|column|block}
+void sudoku_update_INTERNAL(Sudoku* s, Cell* (*getter)(Sudoku* _s, int _group_index, int _cell_index), int index)
+{
+	int i, j;
+	int v;
+
+	for (i = 0; i < s->value_range; i++)
+	{
+		v = cell_get_unique_value(getter(s, index, i));
+
+		if (v != 0)
+		{
+			for (j = 0; j < s->value_range; j++)
+			{
+				if (i != j)
+					cell_unset_value(getter(s, index, j), v);
+			}
+		}
+	}
+}
+
+// Met à jour les valeurs possibles pour une ligne donnée
+inline void sudoku_update_row(Sudoku* s, int index)
+{
+	sudoku_update_INTERNAL(s, &sudoku_get_row_cell, index);
+}
+
+// Met à jour les valeurs possibles pour une colonne donnée
+inline void sudoku_update_column(Sudoku* s, int index)
+{
+	sudoku_update_INTERNAL(s, &sudoku_get_column_cell, index);
+}
+
+// Met à jour les valeurs possibles pour un bloc donné
+inline void sudoku_update_block(Sudoku* s, int index)
+{
+	sudoku_update_INTERNAL(s, &sudoku_get_block_cell, index);
+}
+
+// MMMMAAAAAAAAIIIINNNN
+int main(int argc, char* argv[])
+{
+	int i;
+	Sudoku* s;
+
+	s = sudoku_create(stdin);
+	if (s == NULL) { fprintf(stderr, "Sudoku initialization failed\n"); return EXIT_FAILURE; }
+
+	sudoku_print(s);
+
+	while (1)
+	{
+		for (i = 0; i < s->value_range; i++)
+		{
+			sudoku_update_row(s, i);
+			sudoku_update_column(s, i);
+			sudoku_update_block(s, i);
+		}
+		sudoku_print(s);
+	}
+
+	sudoku_free(s);
+
+	return EXIT_SUCCESS;
+}
